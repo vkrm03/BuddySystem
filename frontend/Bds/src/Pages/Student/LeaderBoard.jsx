856d9adf61c36e8d78fbe29ee99d84b802ea3b95
vikram <vikramrokith03@gmail.com>
2025-02-04 21:09:44 +0530

    fixed

--- frontend/Bds/src/Pages/Student/LeaderBoard.jsx
+++ frontend/Bds/src/Pages/Student/LeaderBoard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../../styles/Leaderboard.css";
+import "../../styles/LeaderBoard.css";
 
 const Leaderboard = () => {
   const [selectedWeek, setSelectedWeek] = useState(1); // Default week 1
